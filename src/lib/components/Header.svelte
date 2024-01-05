0062624e90230aea98e82df3138ff7876ac2c0c7
Abydos <38505162+Abydos12@users.noreply.github.com>
2024-01-05 01:40:45 -0500

    header improvments

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,10 +2,10 @@
 	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
 </script>
 
-<header class="sticky z-10 top-0 flex sm:grid sm:grid-cols-3 gap-2 items-start">
-	<div class="glass p-4 text-center">
-		<h1 class="text-3xl md:text-4xl font-medium mb-2 col-start-1">Guillaume Berdeaux</h1>
-		<h2 class="text-lg md:text-xl font-mono">Software Engineer</h2>
+<header class="sticky z-10 top-0 flex justify-between sm:grid sm:grid-cols-3 gap-2 items-start">
+	<div class="glass p-4 text-center col-start-2">
+		<h1 class="text-lg sm:text-xl md:text-4xl font-medium mb-2">Guillaume Berdeaux</h1>
+		<h2 class="text-sm sm:text-lg md:text-xl font-mono">Software Engineer</h2>
 	</div>
 
 	<div class="glass p-2 md:p-4 justify-self-end">
