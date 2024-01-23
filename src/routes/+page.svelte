4c085736e9dec8eefdcbadb296725c3b36b0cb1a
Abydos <38505162+Abydos12@users.noreply.github.com>
2024-01-23 18:09:49 -0500

    add home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,43 @@
 </script>
 
 <div class="h-full flex flex-col items-center justify-center gap-4">
-  <div class="glass p-4 lg:p-8 w-full lg:w-5/6 flex flex-col">
-    <div class="text-3xl lg:text-6xl">Guillaume Berdeaux</div>
-    <div class="text-lg lg:text-3xl font-mono">Software Engineer</div>
-    <p class="flex-1 mt-4 text-lg">I'm a passionate Software Engineer, TODO</p>
+  <div class="glass p-4 lg:p-8 w-full lg:w-5/6 flex flex-col text-lg gap-4">
+    <div>
+      <div class="text-3xl lg:text-6xl">Guillaume Berdeaux</div>
+      <div class="lg:text-3xl font-mono">Software Engineer</div>
+    </div>
+
+    <p>
+      I'm a passionate Software Engineer and computer technologies enthusiast. I
+      love to try and experiment new technologies. Constantly seeking new ways
+      to build and improve software.
+    </p>
+    <ul class="list-square list-inside">
+      I built this website for two main reasons:
+      <li>
+        Learn Svelte, Tailwindcss and show some of my skills about web development, self-hosting and deployment
+      </li>
+      <li>
+        Have a online CV
+      </li>
+    </ul>
+    <a href="/skills#cv" class="underline hover:decoration-red-600">
+      Curious about how I built and deployed this app, this way...
+    </a>
+
   </div>
 
   <div class="flex flex-col gap-2 max-md:w-5/6">
     <small>Want my CV in PDF ? this way...</small>
     <div class="flex max-md:flex-col gap-2">
       {#each cv_links as link}
-        <div class="glass text-center divide-y divide-zinc-500/40 dark:divide-white/20">
+        <div
+          class="glass text-center divide-y divide-zinc-500/40 dark:divide-white/20"
+        >
           <div class="p-2">{link.lang}</div>
           <div
             class="divide-x divide-zinc-500/40 dark:divide-white/20 grid grid-cols-2"
           >
-
             <a
               href={link.url}
               target="_blank"
@@ -45,6 +66,5 @@
         </div>
       {/each}
     </div>
-
   </div>
 </div>
