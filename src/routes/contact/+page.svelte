5b3cb5009bcd89c2d547eab55cb3c444dbea8ea2
Abydos <38505162+Abydos12@users.noreply.github.com>
2024-01-23 23:43:54 -0500

    change text size contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,21 +4,21 @@
 </script>
 
 <div
-  class="h-full flex flex-col justify-center text-center gap-4 lg:text-2xl lg:w-2/6 xl:w-2/12 mx-auto"
+  class="h-full flex flex-col justify-center text-center gap-4 lg:text-3xl lg:w-3/6 xl:w-3/12 mx-auto"
 >
   <a
-    class="glass p-4 font-mono text-lg hover:underline flex items-center justify-center gap-2"
+    class="glass p-4 font-mono hover:underline flex items-center justify-center gap-2"
     href="mailto:contact@gbdx.fr"
   >
-    <Email class="w-6 h-6 inline-block" />
+    <Email class="w-6 lg:w-8 h-6 lg:h-8 inline-block" />
     <span>contact@gbdx.fr</span>
   </a>
   <a
-    class="glass p-4 font-mono text-lg hover:underline flex items-center justify-center gap-2"
+    class="glass p-4 font-mono hover:underline flex items-center justify-center gap-2"
     href="https://www.linkedin.com/in/guillaume-bdx"
     target="_blank"
   >
-    <img alt="Linkedin logo" class="h-6 inline-block" src={linkedinLogo} />
+    <img alt="Linkedin logo" class="w-6 lg:w-8 h-6 lg:h-8 inline-block" src={linkedinLogo} />
     <span>Linkedin</span>
   </a>
 </div>
